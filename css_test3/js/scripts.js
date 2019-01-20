1615b260affe146e62fb2cad482b6c97d50be937
Stanislau Shliakhtsich <stasgm@gmail.com>
2019-01-20 14:11:11 +0300

    wip

--- css_test3/js/scripts.js
+++ css_test3/js/scripts.js
@@ -1,75 +1,47 @@
+import { getList } from "./tree-fata/world.js";
+
 function addClass() {
-  // const elm = this.parentElement.querySelector(".nested");
   this.parentElement.querySelector(".nested").classList.toggle("active");
   this.classList.toggle("fa-plus-square");
   this.classList.toggle("fa-minus-square");
 }
 
-/* const list = [
-  {
-    name: "1",
-    children: [
-      {
-        name: "1.1",
-        children: [
-          {
-            name: "1.1.1",
-            children: []
-          },
-          {
-            name: "1.1.2",
-            children: []
-          }
-        ]
-      },
-      {
-        name: "1.2",
-        children: [
-          {
-            name: "1.2.1",
-            children: []
-          },
-          {
-            name: "1.2.2",
-            children: [
-              {
-                name: "1.2.2.1",
-                children: []
-              },
-              {
-                name: "1.2.2.2",
-                children: []
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: "2",
-    children: [
-      {
-        name: "2.1",
-        children: []
-      },
-      {
-        name: "2.2",
-        children: [
-          {
-            name: "2.2.1",
-            children: []
-          },
-          {
-            name: "2.2.2",
-            children: []
-          }
-        ]
-      }
-    ]
-  }
-];
- */
+function ExpandTree() {
+  document.querySelectorAll(".nested").forEach(node => {
+    if (!node.classList.contains("active")) {
+      node.classList.add("active");
+    }
+  });
+
+  document.querySelectorAll(".icon").forEach(node => {
+    if (!node.classList.contains("fa-minus-square")) {
+      node.classList.remove("fa-plus-square");
+      node.classList.add("fa-minus-square");
+    }
+  });
+}
+
+function collapseTree() {
+  document.querySelectorAll(".nested").forEach(node => {
+    if (node.classList.contains("active")) {
+      node.classList.remove("active");
+    }
+  });
+
+  document.querySelectorAll(".icon").forEach(node => {
+    if (node.classList.contains("fa-minus-square")) {
+      node.classList.add("fa-plus-square");
+      node.classList.remove("fa-minus-square");
+    }
+  });
+}
+
+// buttons event handlers
+document
+  .querySelector("#collapse-tree")
+  .addEventListener("click", collapseTree);
+document.querySelector("#expand-tree").addEventListener("click", ExpandTree);
+
 const addChild = (list, parent) => {
   list.forEach(i => {
     // Добавляем нод - li
@@ -80,7 +52,7 @@ const addChild = (list, parent) => {
     // добавляем стрелку (если есть дети)
     if (i.children && i.children.length > 0) {
       const icon = document.createElement("i");
-      icon.className = "fa fa-minus-square";
+      icon.className = "icon fa fa-plus-square";
       icon.onclick = addClass;
       treeNode.appendChild(icon);
     }
@@ -101,7 +73,7 @@ const addChild = (list, parent) => {
     // добавляем детей (если есть дети)
     if (i.children && i.children.length > 0) {
       const nested = document.createElement("ul");
-      nested.className = "nested active";
+      nested.className = "nested";
       treeNode.appendChild(nested);
 
       addChild(i.children, nested);
@@ -109,11 +81,12 @@ const addChild = (list, parent) => {
   });
 };
 
-getChildren = (level, num) => {
+const getChildren = (level, num) => {
   const children = [];
 
   const maxChild = (Math.random() * 5 + 1) << 0;
-  if (level < 10) {
+  const maxLevel = (Math.random() * 10 + 1) << 0;
+  if (level < maxLevel) {
     for (let i = 0; i < maxChild; i += 1) {
       children.push(getChildren(++level, `${num}.${i + 1}`));
     }
@@ -125,12 +98,15 @@ getChildren = (level, num) => {
   };
 };
 
-let list = [getChildren(0, 1)];
-
-const root = document.getElementById("root");
+const root = document.getElementById("content");
 
 const ul = document.createElement("ul");
 ul.id = "myUL";
 root.appendChild(ul);
 
-addChild(list, ul);
+// let list = [getChildren(0, 1)];
+let list = [];
+getList().then(res => {
+  list = res;
+  addChild(list, ul);
+});
