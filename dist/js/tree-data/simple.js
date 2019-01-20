4002ffbd8a6f41fde7200a6cd8ee34cd2644e1a9
Stanislau Shliakhtsich <stasgm@gmail.com>
2019-01-20 14:23:21 +0300

    wip

---
+++ dist/js/tree-data/simple.js
@@ -0,0 +1,64 @@
+export const list = [
+  {
+    name: "1",
+    children: [
+      {
+        name: "1.1",
+        children: [
+          {
+            name: "1.1.1",
+            children: []
+          },
+          {
+            name: "1.1.2",
+            children: []
+          }
+        ]
+      },
+      {
+        name: "1.2",
+        children: [
+          {
+            name: "1.2.1",
+            children: []
+          },
+          {
+            name: "1.2.2",
+            children: [
+              {
+                name: "1.2.2.1",
+                children: []
+              },
+              {
+                name: "1.2.2.2",
+                children: []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "2",
+    children: [
+      {
+        name: "2.1",
+        children: []
+      },
+      {
+        name: "2.2",
+        children: [
+          {
+            name: "2.2.1",
+            children: []
+          },
+          {
+            name: "2.2.2",
+            children: []
+          }
+        ]
+      }
+    ]
+  }
+];
